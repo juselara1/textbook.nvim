e01b46329d9f905e98f245a04c96ecd3659bb793
Juan Lara <julara@unal.edu.co>
2023-05-02 17:36:11 -0500

    Changed rendered ranges

--- src/textbook_nvim/render.py
+++ src/textbook_nvim/render.py
@@ -6,12 +6,15 @@ from typing import Dict, List, Tuple, Type
 from rich.console import Console
 from rich.syntax import Syntax
 from rich.markdown import Markdown
+from rich.panel import Panel
+from pathlib import Path
 
 class RenderedCell(BaseModel):
     text: str
     cell_range: Tuple[int, int]
 
-RenderedText = List[RenderedCell]
+class RenderedText(BaseModel):
+    values: List[RenderedCell]
 
 class AbstractRender(ABC):
     def __init__(self, lexer: str = "python", comment_pattern: str="^#"):
@@ -33,13 +36,16 @@ class CodeRender(AbstractRender):
     def render(self) -> RenderedCell:
         lines = self.parsed_cell.text.split("\n")[1:]
         clean_text = "\n".join(lines)
+        syntax = Syntax(clean_text, self.lexer)
         with self.console.capture() as capture:
-            syntax = Syntax(clean_text, self.lexer)
             self.console.print(syntax)
+            self.console.print(" ")
         result = capture.get()
+        self.console.print(syntax)
+        self.console.print(" ")
         return RenderedCell(
                 text=result,
-                cell_range=(self.init_pos, self.init_pos + len(result.split("\n")))
+                cell_range=(self.init_pos, self.init_pos + len(result.split("\n")) - 2)
                 )
 
 class MarkdownRender(AbstractRender):
@@ -48,14 +54,14 @@ class MarkdownRender(AbstractRender):
         lines = self.parsed_cell.text.split("\n")[1:]
         clean_lines = map(lambda line: re.sub(self.comment_pattern, "", line), lines)
         clean_text = "\n".join(clean_lines)
+        md = Panel(Markdown(clean_text), title="markdown")
         with self.console.capture() as capture:
-            md = Markdown(clean_text)
             self.console.print(md)
         rendered_lines = capture.get().split("\n")
-        result = "\n".join(map(lambda line: self.comment_pattern[1:] + line, rendered_lines))
+        self.console.print(md)
         return RenderedCell(
-                text=result,
-                cell_range=(self.init_pos, self.init_pos + len(rendered_lines))
+                text="\n".join(rendered_lines),
+                cell_range=(self.init_pos, len(rendered_lines) - 1)
                 )
 
 class Renderer:
@@ -83,16 +89,23 @@ class Renderer:
         return self
     
     def render(self) -> "Renderer":
-        self.rendered_text = []
-        init_pos = 0
-        for cell in self.parsed_text:
-            self.rendered_text.append(
+        self.rendered_text = RenderedText(values=[])
+        init_pos = 1
+        for cell in self.parsed_text.values:
+            self.rendered_text.values.append(
                     self.renders[cell.cell_type](lexer=self.lexer)
                     .setup(cell, self.console, init_pos)
                     .render()
                     )
-            init_pos += len(self.rendered_text[-1].text.split("\n"))
+            init_pos = self.rendered_text.values[-1].cell_range[1] + 1
         return self
 
     def get_rendered_text(self) -> RenderedText:
         return self.rendered_text
+
+    def save(self, path: Path) -> None:
+        with open(path, "w") as f:
+            f.write(self.rendered_text.json())
+
+    def load(self, path: Path) -> None:
+        self.rendered_text = RenderedText.parse_file(path)
